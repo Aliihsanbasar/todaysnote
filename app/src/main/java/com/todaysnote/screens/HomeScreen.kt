c2ad8fc88150f3407a98506331da9c452cb0399c
Aliihsanbasar <aliihsanbasar@gmail.com>
2024-02-19 01:13:03 +0300

    HomeScreen search textfield added

--- app/src/main/java/com/todaysnote/screens/HomeScreen.kt
+++ app/src/main/java/com/todaysnote/screens/HomeScreen.kt
@@ -3,6 +3,11 @@ package com.todaysnote.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,22 +16,40 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import com.todaysnote.ui.theme.PurplePrimary
 
 @Composable
-fun HomeScreen(navController: NavHostController){
+fun HomeScreen(navController: NavHostController) {
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.White),
-        contentAlignment = Alignment.Center){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
 
-        Text("Home Screen")
+        OutlinedTextField(value = "",
+            onValueChange = {},
+            singleLine = true,
+            placeholder = {
+                Text(text = "safdasdf")
+            },
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = "Search", tint = PurplePrimary)
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedTextColor = PurplePrimary,
+                unfocusedTextColor = Color.Gray,
+                focusedBorderColor = PurplePrimary,
+                unfocusedBorderColor = PurplePrimary
+            )
+        )
     }
 }
 
 
 @Preview
 @Composable
-fun PreviewHomeScreen(){
+fun PreviewHomeScreen() {
     HomeScreen(navController = NavHostController(LocalContext.current))
 }
